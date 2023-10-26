d5ebd797857a3c43bc43429f167b4d35db50dde6
m-kichik <kichik.mg@phystech.edu>
2023-10-26 10:35:29 +0300

    homework 5 v1.0

--- 05/LRU_cache.py
+++ 05/LRU_cache.py
@@ -1,42 +1,12 @@
-import copy
 from typing import Any, Hashable
 
-# class Node:
-#     def __init__(self, value: Hashable = None, left: Any = None, right: Any = None) -> None:
-#         self.__value = value
-#         self.__left = left
-#         self.__right = right
-
-#     def set_value(self, value: Hashable) -> None:
-#         self.__value = value
-
-#     def get_value(self) -> Hashable:
-#         return self.__value
-
-#     def set_left(self, other):
-#         if isinstance(other, Node) or other is None:
-#             self.__left = other
-#         else:
-#             raise TypeError('Other have to be Node or None')
-        
-#     def set_right(self, other):
-#         if isinstance(other, Node) or other is None:
-#             self.__right = other
-#         else:
-#             raise TypeError('Other have to be Node or None')
-        
-#     def get_left(self):
-#         return self.__left
-    
-#     def get_right(self):
-#         return self.__right
 
 class TwoWayList:
     class Node:
         def __init__(self,
                      value: Hashable = None,
-                     left: Any = None, right:
-                     Any = None):
+                     left: Any = None,
+                     right: Any = None):
             self.value = value
             self.left = left
             self.right = right
@@ -52,50 +22,37 @@ class TwoWayList:
             self.tail = new_node
         else:
             new_node.right = self.head
-            # new_node.set_right(self.head)
             self.head.left = new_node
-            # self.head.set_left(new_node)
             self.head = new_node
         return new_node
 
     def pop_last(self) -> Hashable:
         if not self.tail:
-            raise IndexError('Can not pop item from empty list')
+            raise IndexError("Can not pop item from empty list")
         if self.head == self.tail:
             value = self.head.value
-            # value = self.head.get_value()
             self.head = None
             self.tail = None
         else:
             value = self.tail.value
-            # value = self.tail.get_value()
             self.tail = self.tail.left
-            # self.tail = self.tail.get_left()
             self.tail.right = None
-            # self.tail.set_right(None)
         return value
 
-    def move2top(self, node:Node) -> None:
+    def move2top(self, node: Node) -> None:
         if node is self.head:
             return
-        
+
         if node is self.tail:
             self.tail = node.left
-            # self.tail = node.get_left()
             self.tail.right = None
-            # self.tail.set_right(None)
         else:
             node.left.right = node.right
-            # node.get_left().set_right(node.get_right())
             node.right.left = node.left
-            # node.get_right().set_left(node.get_left())
-        
+
         node.right = self.head
-        # node.set_right(self.head)
         node.left = None
-        # node.set_left(None)
         self.head.left = node
-        # self.head.set_left(node)
         self.head = node
 
     def to_list(self):
@@ -103,43 +60,48 @@ class TwoWayList:
         current = self.head
         while current:
             result.append(current.value)
-            # result.append(current.get_value())
             current = current.right
-            # current = current.get_right()
         return result
+
 
 class LRUCache:
     def __init__(self, limit: int = 42):
-        self.limit = limit
-        self.n_items = 0
-        self.queue = TwoWayList()
-        self.items_dict = dict()
+        self.__limit = limit
+        self.__n_items = 0
+        self.__queue = TwoWayList()
+        self.__items_dict = {}
 
     def get(self, key):
-        if not key in self.items_dict:
+        if not isinstance(key, Hashable):
+            raise TypeError(f"Unhashable type: {type(key)}")
+
+        if key not in self.__items_dict:
             return None
-        self.queue.move2top(self.items_dict[key]['list_in_queue'])
-        return self.items_dict[key]['value']
+        self.__queue.move2top(self.__items_dict[key]["list_in_queue"])
+        return self.__items_dict[key]["value"]
 
     def __set_value(self, key, value):
-        list_in_queue = self.queue.add2top(key)
-        self.items_dict[key] = {
-                'value' : value,
-                'list_in_queue' : list_in_queue
+        list_in_queue = self.__queue.add2top(key)
+        self.__items_dict[key] = {
+            "value": value,
+            "list_in_queue": list_in_queue
             }
-    
-    def set(self, key: Any, value: Any):
-        if key in self.items_dict:
-            self.queue.move2top(self.items_dict[key]['list_in_queue'])
-            self.items_dict[key]['value'] = value
+
+    def set(self, key: Hashable, value: Any):
+        if not isinstance(key, Hashable):
+            raise TypeError(f"Unhashable type: {type(key)}")
+
+        if key in self.__items_dict:
+            self.__queue.move2top(self.__items_dict[key]["list_in_queue"])
+            self.__items_dict[key]["value"] = value
         else:
-            if self.n_items == self.limit:
-                key2remove = self.queue.pop_last()
-                self.items_dict.pop(key2remove)
+            if self.__n_items == self.__limit:
+                key2remove = self.__queue.pop_last()
+                self.__items_dict.pop(key2remove)
                 self.__set_value(key, value)
             else:
                 self.__set_value(key, value)
-                self.n_items += 1
+                self.__n_items += 1
 
 
 def main():
---
+++ 05/test_LRU_cache.py
@@ -0,0 +1,195 @@
+import unittest
+
+from LRU_cache import TwoWayList, LRUCache
+
+
+class TestTwoWayList(unittest.TestCase):
+    def test_empty_list(self):
+        twoway_list = TwoWayList()
+        self.assertIsNone(twoway_list.head)
+        self.assertIsNone(twoway_list.tail)
+        self.assertEqual(twoway_list.to_list(), [])
+
+    def test_add2top(self):
+        twoway_list = TwoWayList()
+
+        with self.subTest("Test add first value"):
+            twoway_list.add2top("first")
+
+            self.assertIs(twoway_list.head, twoway_list.tail)
+            self.assertEqual(twoway_list.head.value, "first")
+            self.assertIsNone(twoway_list.head.left)
+            self.assertIsNone(twoway_list.head.right)
+
+        with self.subTest("Test add second value"):
+            twoway_list.add2top("second")
+
+            self.assertEqual(twoway_list.head.value, "second")
+            self.assertEqual(twoway_list.tail.value, "first")
+            self.assertIsNone(twoway_list.head.left)
+            self.assertIs(twoway_list.head.right, twoway_list.tail)
+            self.assertIs(twoway_list.head, twoway_list.tail.left)
+            self.assertIsNone(twoway_list.tail.right)
+
+        with self.subTest("Test add third to have smth between head and tail"):
+            twoway_list.add2top("third")
+
+            self.assertEqual(twoway_list.head.value, "third")
+            self.assertEqual(twoway_list.tail.value, "first")
+            self.assertIsNone(twoway_list.head.left)
+            self.assertIs(twoway_list.head.right, twoway_list.tail.left)
+            self.assertIs(twoway_list.head.right, twoway_list.tail.left)
+            self.assertIsNone(twoway_list.tail.right)
+
+    def test_to_list(self):
+        twoway_list = TwoWayList()
+
+        with self.subTest("Test empty"):
+            self.assertEqual(twoway_list.to_list(), [])
+
+        twoway_list.add2top("first")
+        twoway_list.add2top("second")
+        twoway_list.add2top("third")
+
+        with self.subTest("Test not empty"):
+            self.assertEqual(
+                twoway_list.to_list(),
+                ["third", "second", "first"]
+                )
+
+    def test_pop_last(self):
+        twoway_list = TwoWayList()
+        twoway_list.add2top("first")  # tail
+        twoway_list.add2top("second")  # middle
+        twoway_list.add2top("third")  # head
+
+        with self.subTest("Test pop item"):
+            self.assertEqual(twoway_list.pop_last(), "first")
+
+            self.assertEqual(twoway_list.to_list(), ["third", "second"])
+            self.assertEqual(twoway_list.tail.value, "second")
+            self.assertIsNone(twoway_list.tail.right)
+
+        with self.subTest("Test pop tail"):
+            self.assertEqual(twoway_list.pop_last(), "second")
+
+            self.assertEqual(twoway_list.to_list(), ["third"])
+            self.assertEqual(twoway_list.tail.value, "third")
+            self.assertIs(twoway_list.tail, twoway_list.head)
+            self.assertIsNone(twoway_list.head.left)
+            self.assertIsNone(twoway_list.head.right)
+
+        with self.subTest("Test pop head"):
+            self.assertEqual(twoway_list.pop_last(), "third")
+
+            self.assertEqual(twoway_list.to_list(), [])
+            self.assertIsNone(twoway_list.head)
+            self.assertIsNone(twoway_list.tail)
+
+        with self.subTest("Test pop empty"):
+            with self.assertRaises(IndexError):
+                twoway_list.pop_last()
+
+    def test_move2top(self):
+        twoway_list = TwoWayList()
+        node_with_first = twoway_list.add2top("first")
+        node_with_second = twoway_list.add2top("second")
+        node_with_third = twoway_list.add2top("third")
+
+        with self.subTest("Test move from middle"):
+            twoway_list.move2top(node_with_second)
+
+            self.assertEqual(
+                twoway_list.to_list(),
+                ["second", "third", "first"]
+                )
+            self.assertIs(twoway_list.head, node_with_second)
+            self.assertIs(twoway_list.head.right, node_with_third)
+            self.assertIs(twoway_list.tail.left, node_with_third)
+            self.assertIs(twoway_list.tail, node_with_first)
+
+        with self.subTest("Test move head"):
+            twoway_list.move2top(node_with_second)
+
+            self.assertEqual(
+                twoway_list.to_list(),
+                ["second", "third", "first"]
+                )
+            self.assertIs(twoway_list.head, node_with_second)
+            self.assertIs(twoway_list.tail, node_with_first)
+            self.assertIs(twoway_list.head.right, node_with_third)
+            self.assertIs(twoway_list.tail.left, node_with_third)
+
+        with self.subTest("Test move tail"):
+            twoway_list.move2top(node_with_first)
+
+            self.assertEqual(
+                twoway_list.to_list(),
+                ["first", "second", "third"]
+                )
+            self.assertIs(twoway_list.head, node_with_first)
+            self.assertIs(twoway_list.tail, node_with_third)
+            self.assertIs(twoway_list.head.right, node_with_second)
+            self.assertIs(twoway_list.tail.left, node_with_second)
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_set_and_get(self):
+        cache = LRUCache(4)
+
+        with self.subTest("Test set incorrect key"):
+            with self.assertRaises(TypeError):
+                cache.set(["k1"], 1)
+
+        with self.subTest("Test get incorrect key"):
+            with self.assertRaises(TypeError):
+                cache.get(["k1"])
+
+        with self.subTest("Test get from empty cache"):
+            self.assertIsNone(cache.get("k1"))
+
+        with self.subTest("Test set and get correct key"):
+            cache.set("k1", 1)
+            self.assertEqual(cache.get("k1"), 1)
+
+        with self.subTest("Test set new value"):
+            cache.set("k1", 11)
+            self.assertEqual(cache.get("k1"), 11)
+
+        with self.subTest("Test get with non-existed key"):
+            self.assertIsNone(cache.get(42))
+
+        with self.subTest("Test set multiple items"):
+            cache.set("k2", 2)
+            cache.set("k3", 3)
+            cache.set("k4", 4)
+
+            self.assertEqual(cache.get("k2"), 2)
+            self.assertEqual(cache.get("k3"), 3)
+            self.assertEqual(cache.get("k4"), 4)
+
+        with self.subTest("Test set more than cache capacity"):
+            cache.set("k5", 5)  # ['k5', 'k4', 'k3', 'k2']
+
+            self.assertEqual(cache.get("k5"), 5)
+            self.assertIsNone(cache.get("k1"))
+
+        with self.subTest("Test 'get' moves to top"):
+            self.assertEqual(cache.get("k2"), 2)  # ['k2', 'k5', 'k4', 'k3']
+
+            cache.set("k6", 6)  # ['k6', 'k2', 'k5', 'k4']
+
+            self.assertEqual(cache.get("k6"), 6)
+            self.assertEqual(cache.get("k3"), None)
+
+        with self.subTest("Test 'set' new value moves to top"):
+            cache.set("k4", 44)  # ['k4', 'k6', 'k2', 'k5']
+            cache.set("k7", 7)  # ['k7', 'k4', 'k6', 'k2']
+
+            self.assertIsNone(cache.get("k5"))
+            self.assertEqual(cache.get("k4"), 44)
+            self.assertEqual(cache.get("k7"), 7)
+
+
+if __name__ == "__main__":
+    unittest.main()
